37dd7e26a4309f1b202732cd163b53da49948b6c
Bang <hb398@cornell.edu>
2017-09-28 22:13:11 -0400

    spawn turtle at random loc

--- social_nav/scripts/pedestrians.py
+++ social_nav/scripts/pedestrians.py
@@ -1,31 +1,62 @@
 #!/usr/bin/env python
 
+
 import rospy
-import turtlesim.srv
-import geometry_msgs.msg
-import tf
-import math
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+
 import roslib
+import math
+import tf
+import geometry_msgs.msg
+import turtlesim.srv
+
+import random
+
+
+NUM_PEDESTRIANS = 3
 
 def talker():
+    
+#    pubs = []
+    
+#    for i in range(num_pedestrians):
+#        # Create separate publishers for each turtle pedestrian
+#        pub = rospy.Publisher('/turtle{0}/cmd_vel'.format(i), Twist, queue_size=10)
+#        pubs.append(pub)
+    
+    
+    # 1. Generate nodes: publisher and subscriber for each turtle. 
+    # 2. 
+    
+    
+    rospy.init_node('pedestrians', anonymous=True)
+    
+    #listener = tf.TransformListener()
 
-    rospy.init_node('pedestrians',anonymous=True)
     rospy.wait_for_service('spawn')
     spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
+    spawner(random.random(), random.random(), random.random(), 'turtle1')
+    
+#    
+#    for i in range(NUM_PEDESTRIANS):
+#        spawner(random.random(), random.random(), random.random(), 'turtle{0}'.format(i))
 
-    rate = rospy.Rate(10) # 10 hz
+    rate = rospy.Rate(10) # 10hz
 
     while not rospy.is_shutdown():
         
         rospy.loginfo("hello world")
-
+        
         rate.sleep()
+
+            
 
 
 if __name__ == '__main__':
-
     try:
         talker()
-
     except rospy.ROSInterruptException:
         pass
+
